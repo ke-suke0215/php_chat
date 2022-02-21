0aa2cbb4f2a54adcf09464d5c46e357e9486912c
ke-suke0215 <keisuke.yohs.0215.1209@gmail.com>
2022-02-21 09:10:16 +0900

    index, login, join/index のHTML作成

---
+++ join/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <h1>アカウント登録画面</h1>
+  <form action="" method="post">
+    <label>
+      <p>名前</p>
+      <input type="text" name="name" size="35" value=""/>
+    </label>
+    <label>
+      <p>メールアドレス</p>
+      <input type="text" name="email" size="35" value=""/>
+    </label>
+    <label>
+      <p>パスワード</p>
+      <input type="text" name="password" size="35" value=""/>
+    </label>
+    <input type="submit" value="内容確認">
+  </form>
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <h1>アカウント登録画面</h1>
+  <form action="" method="post">
+    <label>
+      <p>メールアドレス</p>
+      <input type="text" name="email" size="35" value=""/>
+    </label>
+    <label>
+      <p>パスワード</p>
+      <input type="text" name="password" size="35" value=""/>
+    </label>
+    <input type="submit" value="ログイン">
+  </form>
+</body>
+</html>
